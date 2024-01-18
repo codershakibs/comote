eca325a0a9579bb0e00342d282553cee683032ce
shakib <codershakibs@gmail.com>
2024-01-18 12:33:42 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
-var splide = new Splide('.splide', {
-    perPage: 2,
-    gap: 30,
-    rewind: true,
-    breakpoints: {
-        350: { perPage: 1, gap: '1rem' },
-        640: { perPage: 2, gap: 0 },
-    },
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-splide.mount();
-
-
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-    },
-
-
-});
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
