660aa557bc9cc82e41db1e882495b1861eac816c
shakib <codershakibs@gmail.com>
2024-02-02 16:27:05 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,5 +59,64 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    let splideElements = document.querySelectorAll(".why-choose");
+
+    splideElements.forEach(function (element) {
+        let splide = new Splide(element, {
+            type: "loop",
+            perPage: 3,
+            perMove: 1,
+            gap: "40px",
+            pagination: true,
+            breakpoints: {
+                1350: {
+                    gap: "20px",
+                },
+                991: {
+                    perPage: 2,
+                    gap: "15px",
+                },
+                575: {
+                    perPage: 1,
+                    gap: "10px",
+                },
+            },
+        });
+
+        splide.mount();
+    });
+});
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let splideElements = document.querySelectorAll(".client");
+
+    splideElements.forEach(function (element) {
+        let splide = new Splide(element, {
+            type: "loop",
+            perPage: 3,
+            perMove: 1,
+            gap: "40px",
+            pagination: true,
+            breakpoints: {
+                1350: {
+                    gap: "20px",
+                },
+                991: {
+                    perPage: 2,
+                    gap: "15px",
+                },
+                586: {
+                    perPage: 1,
+                    gap: "10px",
+                },
+            },
+        });
+
+        splide.mount();
+    });
+});
